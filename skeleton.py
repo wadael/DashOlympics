bca001c12cd8a76f324f01341279ae170a97b497
wadael <wadael@gmail.com>
2021-05-17 16:13:05 +0200

    added charts wanted to skeleton

--- skeleton.py
+++ skeleton.py
@@ -12,16 +12,29 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# data from  https://www.kaggle.com/the-guardian/olympic-games
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+# For each sport , the number of olympics this sport is present
+
+# For each sport , in which olympics this sport is present 
+
+#Which country is better in summer/winter ?
+
+#Countries with most medals Total and by discipline
+
+#For each athlete, number of medals in the oympics particpated
+
+
+#Evolution   % men, % women
+
+# for each country, the metal repartition.
+
+#Example of crossing datasets together : finding the datasets of happiness per country, to find out if happy people get medals
+
+
+
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
